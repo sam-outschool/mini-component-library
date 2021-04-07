929283d3379fd8e4d8bac257ddb0fd4f4d5de5b6
Sam Calvert <sam@outschool.com>
2021-04-06 23:37:09 -0700

    Update ProgressBar after watching video

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -19,58 +19,45 @@ const ProgressBar = ({ value, size }) => {
       aria-valuemin={0}
       aria-valuemax={100}
     >
-      <ProgressMask value={value}>
-        <FullProgress />
+      <BarWrapper>
+        <Bar value={value} />
         <VisuallyHidden>{value}%</VisuallyHidden>
-      </ProgressMask>
+      </BarWrapper>
     </Component>
   );
 };
 
 const Wrapper = styled.div`
-  width: 370px;
   background-color: ${COLORS.transparentGray15};
   box-shadow: inset 0px 2px 4px rgba(128, 128, 128, 0.35);
-`;
-
-const ProgressMask = styled.div`
-  width: ${(p) => p.value}%;
-  overflow: hidden;
-`;
-
-const FullProgress = styled.div`
-  background-color: ${COLORS.primary};
-  width: 370px;
   border-radius: 4px;
+  overflow: hidden;
 `;
 
 const LargeWrapper = styled(Wrapper)`
   height: 24px;
   border-radius: 8px;
   padding: 4px;
-
-  ${FullProgress} {
-    width: 362px;
-    height: 16px;
-  }
 `;
 
 const MediumWrapper = styled(Wrapper)`
   height: 12px;
-  border-radius: 4px;
-
-  ${FullProgress} {
-    height: 12px;
-  }
 `;
 
 const SmallWrapper = styled(Wrapper)`
   height: 8px;
-  border-radius: 4px;
+`;
 
-  ${FullProgress} {
-    height: 8px;
-  }
+const BarWrapper = styled.div`
+  overflow: hidden;
+  border-radius: 4px;
+  height: 100%;
+`;
+
+const Bar = styled.div`
+  background-color: ${COLORS.primary};
+  width: ${(p) => p.value}%;
+  height: 100%;
 `;
 
 export default ProgressBar;
